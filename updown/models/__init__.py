a890591f179ea20afa11cf4eab7a1eac94b5ac21
Karan Desai <kddeskar@gmail.com>
2019-07-09 10:09:26 +0530

    Initial commit.

--- updown/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .updown_captioner import UpDownCaptioner
-
-
-__all__ = ["UpDownCaptioner"]
--- updown/modules/__init__.py
+++
@@ -1,5 +0,0 @@
-from .attention import BottomUpTopDownAttention
-from .updown_cell import UpDownCell
-
-
-__all__ = ["BottomUpTopDownAttention", "UpDownCell"]
